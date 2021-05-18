8261de8aa14fe0aced6ed96c689f4e7b177c5b08
MYR <loic.themyr@gmail.com>
2021-05-18 21:18:16 +0200

    utrans

---
+++ UTrans/run/run_all_mhca.py
@@ -0,0 +1,13 @@
+import UTrans.run.run_training_script as run
+
+
+run.main(gpu='2', network='2d', network_trainer='nnUNetTrainerV2_utrans_mhca', task='062', fold=0, outpath='MHCA_v2', val=False)
+run.main(gpu='2', network='2d', network_trainer='nnUNetTrainerV2_utrans_mhca', task='062', fold=0, outpath='MHCA_v2', val=True)
+run.main(gpu='2', network='2d', network_trainer='nnUNetTrainerV2_utrans_mhca', task='062', fold=1, outpath='MHCA_v2', val=False)
+run.main(gpu='2', network='2d', network_trainer='nnUNetTrainerV2_utrans_mhca', task='062', fold=1, outpath='MHCA_v2', val=True)
+run.main(gpu='2', network='2d', network_trainer='nnUNetTrainerV2_utrans_mhca', task='062', fold=2, outpath='MHCA_v2', val=False)
+run.main(gpu='2', network='2d', network_trainer='nnUNetTrainerV2_utrans_mhca', task='062', fold=2, outpath='MHCA_v2', val=True)
+run.main(gpu='2', network='2d', network_trainer='nnUNetTrainerV2_utrans_mhca', task='062', fold=3, outpath='MHCA_v2', val=False)
+run.main(gpu='2', network='2d', network_trainer='nnUNetTrainerV2_utrans_mhca', task='062', fold=3, outpath='MHCA_v2', val=True)
+run.main(gpu='2', network='2d', network_trainer='nnUNetTrainerV2_utrans_mhca', task='062', fold=4, outpath='MHCA_v2', val=False)
+run.main(gpu='2', network='2d', network_trainer='nnUNetTrainerV2_utrans_mhca', task='062', fold=4, outpath='MHCA_v2', val=True)
