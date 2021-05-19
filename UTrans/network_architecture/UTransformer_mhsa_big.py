56909d25b79810458d6c132ad1e2ed864806e70e
MYR <loic.themyr@gmail.com>
2021-05-19 16:59:25 +0200

    utrans

--- UTrans/network_architecture/UTransformer_mhsa_big.py
+++ UTrans/network_architecture/UTransformer_mhsa_big.py
@@ -272,7 +272,7 @@ class UTransformer_mhsa(SegmentationNetwork):
         output_features = base_num_features
         input_features = input_channels
         self.mhsa = []
-        self.do_mhsa = [False, False, False, True, True, True, True] # None, None, None, 64**4, 32**4, 16**4, 8**4
+        self.do_mhsa = [False, False, False, False, True, True, True] # None, None, None, 64**4, 32**4, 16**4, 8**4
         for d in range(num_pool):
             # determine the first stride
             if d != 0 and self.convolutional_pooling:
@@ -522,7 +522,6 @@ class MHSA(nn.Module):
 
         # Positionnal encodding
         print(x.shape)
-        print(self.pe.shape)
         x = x + self.pe.repeat(bs,1,1,1)
 
 
