b4ba12816e0896b7fc8d41e0e5c9c97f8f6c3f5a
MYR <loic.themyr@gmail.com>
2021-05-19 17:27:30 +0200

    utrans

--- UTrans/network_architecture/UTransformer_mhca_big.py
+++ UTrans/network_architecture/UTransformer_mhca_big.py
@@ -335,7 +335,7 @@ class UTransformer_mhca(SegmentationNetwork):
 
         # now lets build the localization pathway
         # strides = [1,1,1,1,2,4]
-        strides = [1,1,1,1,2,2]
+        strides = [1,1,1,1,1,2]
         #          1 2 3 4 5 6
 
         for u in range(num_pool):
