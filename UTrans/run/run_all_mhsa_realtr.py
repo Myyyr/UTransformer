b98e1419615e28dc99c21e1d9f990fb4ce29d286
MYR <loic.themyr@gmail.com>
2021-05-19 18:14:29 +0200

    utrans

--- UTrans/run/run_all_mhsa_realtr.py
+++ UTrans/run/run_all_mhsa_realtr.py
@@ -1,13 +1,13 @@
 import UTrans.run.nnunet_run_training as run
 
 
-run.main(gpu='1', network='2d', network_trainer='nnUNetTrainerV2_utrans_mhsa_realtr', task='062', fold=1, outpath='MHSA_v4', val=False, npz=True)
-run.main(gpu='1', network='2d', network_trainer='nnUNetTrainerV2_utrans_mhsa_realtr', task='062', fold=1, outpath='MHSA_v4', val=True, npz=True)
-run.main(gpu='1', network='2d', network_trainer='nnUNetTrainerV2_utrans_mhsa_realtr', task='062', fold=4, outpath='MHSA_v4', val=False, npz=True)
-run.main(gpu='1', network='2d', network_trainer='nnUNetTrainerV2_utrans_mhsa_realtr', task='062', fold=4, outpath='MHSA_v4', val=True, npz=True)
-run.main(gpu='1', network='2d', network_trainer='nnUNetTrainerV2_utrans_mhsa_realtr', task='062', fold=2, outpath='MHSA_v4', val=False, npz=True)
-run.main(gpu='1', network='2d', network_trainer='nnUNetTrainerV2_utrans_mhsa_realtr', task='062', fold=2, outpath='MHSA_v4', val=True, npz=True)
-run.main(gpu='1', network='2d', network_trainer='nnUNetTrainerV2_utrans_mhsa_realtr', task='062', fold=0, outpath='MHSA_v4', val=False, npz=True)
-run.main(gpu='1', network='2d', network_trainer='nnUNetTrainerV2_utrans_mhsa_realtr', task='062', fold=0, outpath='MHSA_v4', val=True, npz=True)
-run.main(gpu='1', network='2d', network_trainer='nnUNetTrainerV2_utrans_mhsa_realtr', task='062', fold=3, outpath='MHSA_v4', val=False, npz=True)
-run.main(gpu='1', network='2d', network_trainer='nnUNetTrainerV2_utrans_mhsa_realtr', task='062', fold=3, outpath='MHSA_v4', val=True, npz=True)
+run.main(gpu='0', network='2d', network_trainer='nnUNetTrainerV2_utrans_mhsa_realtr', task='062', fold=1, outpath='MHSA_v4', val=False, npz=True)
+run.main(gpu='0', network='2d', network_trainer='nnUNetTrainerV2_utrans_mhsa_realtr', task='062', fold=1, outpath='MHSA_v4', val=True, npz=True)
+run.main(gpu='0', network='2d', network_trainer='nnUNetTrainerV2_utrans_mhsa_realtr', task='062', fold=4, outpath='MHSA_v4', val=False, npz=True)
+run.main(gpu='0', network='2d', network_trainer='nnUNetTrainerV2_utrans_mhsa_realtr', task='062', fold=4, outpath='MHSA_v4', val=True, npz=True)
+run.main(gpu='0', network='2d', network_trainer='nnUNetTrainerV2_utrans_mhsa_realtr', task='062', fold=2, outpath='MHSA_v4', val=False, npz=True)
+run.main(gpu='0', network='2d', network_trainer='nnUNetTrainerV2_utrans_mhsa_realtr', task='062', fold=2, outpath='MHSA_v4', val=True, npz=True)
+run.main(gpu='0', network='2d', network_trainer='nnUNetTrainerV2_utrans_mhsa_realtr', task='062', fold=0, outpath='MHSA_v4', val=False, npz=True)
+run.main(gpu='0', network='2d', network_trainer='nnUNetTrainerV2_utrans_mhsa_realtr', task='062', fold=0, outpath='MHSA_v4', val=True, npz=True)
+run.main(gpu='0', network='2d', network_trainer='nnUNetTrainerV2_utrans_mhsa_realtr', task='062', fold=3, outpath='MHSA_v4', val=False, npz=True)
+run.main(gpu='0', network='2d', network_trainer='nnUNetTrainerV2_utrans_mhsa_realtr', task='062', fold=3, outpath='MHSA_v4', val=True, npz=True)
