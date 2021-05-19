f05b4db44477504db1cc04e64e020d05c2543370
MYR <loic.themyr@gmail.com>
2021-05-19 18:00:31 +0200

    utrans

--- UTrans/run/run_all_mhca_big.py
+++ UTrans/run/run_all_mhca_big.py
@@ -1,5 +1,5 @@
 import UTrans.run.nnunet_run_training as run
 
 
-run.main(gpu='1', network='2d', network_trainer='nnUNetTrainerV2_utrans_mhca_big', task='062', fold=1, outpath='MHCA_v3', val=False, npz=True)
-run.main(gpu='1', network='2d', network_trainer='nnUNetTrainerV2_utrans_mhca_big', task='062', fold=1, outpath='MHCA_v3', val=True, npz=True)
+run.main(gpu='0', network='2d', network_trainer='nnUNetTrainerV2_utrans_mhca_big', task='062', fold=1, outpath='MHCA_v3', val=False, npz=True)
+run.main(gpu='0', network='2d', network_trainer='nnUNetTrainerV2_utrans_mhca_big', task='062', fold=1, outpath='MHCA_v3', val=True, npz=True)
