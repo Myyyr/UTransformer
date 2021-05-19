06c5b1013e6c43b79158909e6835390129efff74
MYR <loic.themyr@gmail.com>
2021-05-19 18:04:49 +0200

    utrans

--- UTrans/network_architecture/UTransformer_mhca_big.py
+++ UTrans/network_architecture/UTransformer_mhca_big.py
@@ -429,8 +429,8 @@ class UTransformer_mhca(SegmentationNetwork):
             if u<len(self.mhca):
                 print(x.shape)
                 x = self.mhca[u](x, skips[-(u + 1)])
-                print(torch.cuda.max_memory_allocated/1024**3)
-                print(torch.cuda.memory_allocated/1024**3)
+                print(torch.cuda.max_memory_allocated()/1024**3)
+                print(torch.cuda.memory_allocated()/1024**3)
             else:
                 x = self.tu[u](x)
                 x = torch.cat((x, skips[-(u + 1)]), dim=1)
