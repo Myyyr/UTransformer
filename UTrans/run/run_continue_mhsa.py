66689b4c8efe1619afc188a61286ba873d5f785c
MYR <loic.themyr@gmail.com>
2021-05-20 17:03:03 +0200

    utrans

--- UTrans/run/run_continue_mhsa.py
+++ UTrans/run/run_continue_mhsa.py
@@ -8,7 +8,7 @@ mod = "MHSA_v2_IN_LeakyReLU/"
 for i in range(1,7):
 	pth = os.path.join(BASE_DIR,mod,MODEL_PATH)
 	print(pth)
-	exit(0)
+	# exit(0)
 	run.main(gpu='1', network='2d', network_trainer='nnUNetTrainerV2_utrans_mhsa', task='062', fold=1, outpath='MHSA_v2_c'+str(i), val=False, npz=True, c=False, ep=50, lr=1e-4, pretrained_weights=pth)
 	run.main(gpu='1', network='2d', network_trainer='nnUNetTrainerV2_utrans_mhsa', task='062', fold=1, outpath='MHSA_v2_c'+str(i), val=True,  npz=True, c=False, ep=50, lr=1e-4 )
 	mod = "MHSA_v2_c"+str(i)+"_IN_LeakyReLU/"
