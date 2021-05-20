3b75b90371012de33fc367988d2a6677bfaa7133
MYR <loic.themyr@gmail.com>
2021-05-20 20:56:59 +0200

    utrans

--- UTrans/run/run_continue_mhca.py
+++ UTrans/run/run_continue_mhca.py
@@ -4,11 +4,11 @@ BASE_DIR="/local/DEEPLEARNING/nnUNetData/nnUNet_trained_models/nnUNet/2d_nnUNetP
 MODEL_PATH = "fold_1/model_final_checkpoint.model"
 
 
-mod = "MHCA_v2_IN_LeakyReLU/"
-for i in range(1,7):
+mod = "MHCA_v2_c1_IN_LeakyReLU/"
+for i in range(2,7):
 	pth = os.path.join(BASE_DIR,mod,MODEL_PATH)
 	print(pth)
 	# exit(0)
 	run.main(gpu='0', network='2d', network_trainer='nnUNetTrainerV2_utrans_mhca', task='062', fold=1, outpath='MHCA_v2_c'+str(i), val=False, npz=True, c=False, ep=50, lr=1e-4, pretrained_weights=pth)
 	run.main(gpu='0', network='2d', network_trainer='nnUNetTrainerV2_utrans_mhca', task='062', fold=1, outpath='MHCA_v2_c'+str(i), val=True,  npz=True, c=False, ep=50, lr=1e-4 )
-	mod = "MHSA_v2_c"+str(i)+"_IN_LeakyReLU/"
+	mod = "MHCA_v2_c"+str(i)+"_IN_LeakyReLU/"
