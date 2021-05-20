e7a577f1c65ecc257e2c778abff59c70501b64be
MYR <loic.themyr@gmail.com>
2021-05-20 17:02:47 +0200

    utrans

--- UTrans/run/run_continue_mhsa.py
+++ UTrans/run/run_continue_mhsa.py
@@ -9,6 +9,6 @@ for i in range(1,7):
 	pth = os.path.join(BASE_DIR,mod,MODEL_PATH)
 	print(pth)
 	exit(0)
-	run.main(gpu='1', network='2d', network_trainer='nnUNetTrainerV2_utrans_mhsa', task='062', fold=1, outpath='MHSA_v2_c'+str(i), val=False, npz=True, c=False, ep=200+50*i, lr=3e-4, pretrained_weights=pth)
-	run.main(gpu='1', network='2d', network_trainer='nnUNetTrainerV2_utrans_mhsa', task='062', fold=1, outpath='MHSA_v2_c'+str(i), val=True,  npz=True, c=False, ep=200+50*i, lr=3e-4 )
+	run.main(gpu='1', network='2d', network_trainer='nnUNetTrainerV2_utrans_mhsa', task='062', fold=1, outpath='MHSA_v2_c'+str(i), val=False, npz=True, c=False, ep=50, lr=1e-4, pretrained_weights=pth)
+	run.main(gpu='1', network='2d', network_trainer='nnUNetTrainerV2_utrans_mhsa', task='062', fold=1, outpath='MHSA_v2_c'+str(i), val=True,  npz=True, c=False, ep=50, lr=1e-4 )
 	mod = "MHSA_v2_c"+str(i)+"_IN_LeakyReLU/"
