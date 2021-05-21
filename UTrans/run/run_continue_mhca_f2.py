870462a7850c0c88e218f9c10a80ed670876738e
MYR <loic.themyr@gmail.com>
2021-05-21 09:36:33 +0200

    utrans

--- UTrans/run/run_continue_mhca_f2.py
+++ UTrans/run/run_continue_mhca_f2.py
@@ -9,6 +9,6 @@ for i in range(1,7):
 	pth = os.path.join(BASE_DIR,mod,MODEL_PATH)
 	print(pth)
 	# exit(0)
-	run.main(gpu='0', network='2d', network_trainer='nnUNetTrainerV2_utrans_mhca', task='062', fold=2, outpath='MHCA_v2_c'+str(i), val=False, npz=True, c=False, ep=50, lr=(1e-4)/i, pretrained_weights=pth)
-	run.main(gpu='0', network='2d', network_trainer='nnUNetTrainerV2_utrans_mhca', task='062', fold=2, outpath='MHCA_v2_c'+str(i), val=True,  npz=True, c=False, ep=50, lr=(1e-4)/i )
+	run.main(gpu='2', network='2d', network_trainer='nnUNetTrainerV2_utrans_mhca', task='062', fold=2, outpath='MHCA_v2_c'+str(i), val=False, npz=True, c=False, ep=50, lr=(1e-4)/i, pretrained_weights=pth)
+	run.main(gpu='2', network='2d', network_trainer='nnUNetTrainerV2_utrans_mhca', task='062', fold=2, outpath='MHCA_v2_c'+str(i), val=True,  npz=True, c=False, ep=50, lr=(1e-4)/i )
 	mod = "MHCA_v2_c"+str(i)+"_IN_LeakyReLU/"
