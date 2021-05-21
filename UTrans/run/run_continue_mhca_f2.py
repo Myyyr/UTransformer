50783543bf03cc9749b65504ba28c3152cfc5b41
MYR <loic.themyr@gmail.com>
2021-05-21 09:35:19 +0200

    utrans

---
+++ UTrans/run/run_continue_mhca_f2.py
@@ -0,0 +1,14 @@
+import UTrans.run.nnunet_run_training as run
+import os
+BASE_DIR="/local/DEEPLEARNING/nnUNetData/nnUNet_trained_models/nnUNet/2d_nnUNetPlansv2.1/Task062_NIHPancreas/"
+MODEL_PATH = "fold_2/model_final_checkpoint.model"
+
+
+mod = "MHCA_v2_c1_IN_LeakyReLU/"
+for i in range(1,7):
+	pth = os.path.join(BASE_DIR,mod,MODEL_PATH)
+	print(pth)
+	# exit(0)
+	run.main(gpu='0', network='2d', network_trainer='nnUNetTrainerV2_utrans_mhca', task='062', fold=2, outpath='MHCA_v2_c'+str(i), val=False, npz=True, c=False, ep=50, lr=(1e-4)/i, pretrained_weights=pth)
+	run.main(gpu='0', network='2d', network_trainer='nnUNetTrainerV2_utrans_mhca', task='062', fold=2, outpath='MHCA_v2_c'+str(i), val=True,  npz=True, c=False, ep=50, lr=(1e-4)/i )
+	mod = "MHCA_v2_c"+str(i)+"_IN_LeakyReLU/"
