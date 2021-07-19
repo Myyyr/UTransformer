9dd0298d728d306072546117173549ba31c23547
MYR <loic.themyr@gmail.com>
2021-07-19 16:30:00 +0200

    segnest

--- UTrans/network_architecture/SegNest.py
+++ UTrans/network_architecture/SegNest.py
@@ -255,7 +255,7 @@ class UTransformer_mhsa(SegmentationNetwork):
     def forward(self, x):
         print(x.shape)
 
-        
+        exit(0)
         seg_outputs = self.segnest(x)
         
 
@@ -313,92 +313,3 @@ class UTransformer_mhsa(SegmentationNetwork):
 
 
 
-
-def cast_tuple(val, depth):
-    return val if isinstance(val, tuple) else ((val,) * depth)
-
-LayerNorm = partial(nn.InstanceNorm2d, affine = True)
-
-# classes
-
-class PreNorm(nn.Module):
-    def __init__(self, dim, fn):
-        super().__init__()
-        self.norm = LayerNorm(dim)
-        self.fn = fn
-
-    def forward(self, x, **kwargs):
-        return self.fn(self.norm(x), **kwargs)
-
-class FeedForward(nn.Module):
-    def __init__(self, dim, mlp_mult = 4, dropout = 0.):
-        super().__init__()
-        self.net = nn.Sequential(
-            nn.Conv2d(dim, dim * mlp_mult, 1),
-            nn.GELU(),
-            nn.Dropout(dropout),
-            nn.Conv2d(dim * mlp_mult, dim, 1),
-            nn.Dropout(dropout)
-        )
-    def forward(self, x):
-        return self.net(x)
-
-class Attention(nn.Module):
-    def __init__(self, dim, heads = 8, dropout = 0.):
-        super().__init__()
-        dim_head = dim // heads
-        inner_dim = dim_head * heads
-        self.heads = heads
-        self.scale = dim_head ** -0.5
-
-        self.attend = nn.Softmax(dim = -1)
-        self.to_qkv = nn.Conv2d(dim, inner_dim * 3, 1, bias = False)
-
-        self.to_out = nn.Sequential(
-            nn.Conv2d(inner_dim, dim, 1),
-            nn.Dropout(dropout)
-        )
-
-    def forward(self, x):
-        b, c, h, w, heads = *x.shape, self.heads
-
-        qkv = self.to_qkv(x).chunk(3, dim = 1)
-        q, k, v = map(lambda t: rearrange(t, 'b (h d) x y -> b h (x y) d', h = heads), qkv)
-
-        dots = einsum('b h i d, b h j d -> b h i j', q, k) * self.scale
-
-        attn = self.attend(dots)
-
-        out = einsum('b h i j, b h j d -> b h i d', attn, v)
-        out = rearrange(out, 'b h (x y) d -> b (h d) x y', x = h, y = w)
-        return self.to_out(out)
-
-def Aggregate(dim, dim_out):
-    return nn.Sequential(
-        nn.Conv2d(dim, dim_out, 3, padding = 1),
-        LayerNorm(dim_out),
-        nn.MaxPool2d(3, stride = 2, padding = 1)
-    )
-
-class Transformer(nn.Module):
-    def __init__(self, dim, seq_len, depth, heads, mlp_mult, dropout = 0.):
-        super().__init__()
-        self.layers = nn.ModuleList([])
-        self.pos_emb = nn.Parameter(torch.randn(seq_len))
-
-        for _ in range(depth):
-            self.layers.append(nn.ModuleList([
-                PreNorm(dim, Attention(dim, heads = heads, dropout = dropout)),
-                PreNorm(dim, FeedForward(dim, mlp_mult, dropout = dropout))
-            ]))
-    def forward(self, x):
-        *_, h, w = x.shape
-
-        pos_emb = self.pos_emb[:(h * w)]
-        pos_emb = rearrange(pos_emb, '(h w) -> () () h w', h = h, w = w)
-        x = x + pos_emb
-
-        for attn, ff in self.layers:
-            x = attn(x) + x
-            x = ff(x) + x
-        return x
