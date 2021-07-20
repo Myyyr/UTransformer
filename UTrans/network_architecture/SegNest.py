95f1314a2d7390d735e9060792ce0b85b00e5d98
MYR <loic.themyr@gmail.com>
2021-07-20 10:46:43 +0200

    segnest

--- UTrans/network_architecture/SegNest.py
+++ UTrans/network_architecture/SegNest.py
@@ -240,8 +240,8 @@ class UTransformer_mhsa(SegmentationNetwork):
         d = h*64
         self.segnest = models.segnest.SegNest(img_size=512, in_chans=1, 
                       patch_size=4, num_levels=4, 
-                      embed_dims=(d, d, d, d), num_heads=(h, h, h, h), 
-                      depths=(3, 3, 3, 3), num_classes=2, 
+                      embed_dims=(d, d, d, d, d, d), num_heads=(h, h, h, h, h, h), 
+                      depths=(3, 3, 3, 3, 3, 3), num_classes=2, 
                       mlp_ratio=4.).float()
 
         self.upscale_logits_ops = []
