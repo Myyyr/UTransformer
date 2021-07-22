d1b6f210e3fcd82102cae4912e776a1ab32be67a
MYR <loic.themyr@gmail.com>
2021-07-22 11:58:50 +0200

    unest - new config

--- UTrans/network_architecture/segtest.py
+++ UTrans/network_architecture/segtest.py
@@ -1,18 +1,28 @@
-import timm
+import timm.models.unest as un
+import timm.models.segnest as se
+
 import torch
 
 
 def main():
+    l = 6
     h = 3
     d = h*64
-    net = timm.models.segnest.SegNest(img_size=512, in_chans=1, 
-                  patch_size=2, num_levels=4, 
-                  embed_dims=(d, d, d, d), num_heads=(h, h, h, h), 
-                  depths=(3, 3, 3, 3), num_classes=14, 
+    net = un.UNest(img_size=512, in_chans=1, 
+                  patch_size=4, num_levels=l, 
+                  embed_dims=(d,)*l, num_heads=(h,)*l, 
+                  depths=(3,)*l, num_classes=14, 
                   mlp_ratio=4.).float()
+    net.eval()
+    # net = se.SegNest(img_size=512, in_chans=1, 
+    #               patch_size=4, num_levels=l, 
+    #               embed_dims=(d,)*l, num_heads=(h,)*l, 
+    #               depths=(3,)*l, num_classes=14, 
+    #               mlp_ratio=4.).float()
     x = torch.rand(1, 1, 512, 512).float()
     print("x", x.shape)
-    y = net(x)
+    with torch.no_grad():
+      y = net(x)
     print("len", len(y))
     for i in range(len(y)):
         print("y",i, y[i].shape)
