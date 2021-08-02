f23078c4f3ee95913ac2c9587edf06f09b099e7b
MYR <loic.themyr@gmail.com>
2021-08-02 10:14:21 +0200

    unest - new config

--- UTrans/network_architecture/UNest3.py
+++ UTrans/network_architecture/UNest3.py
@@ -242,7 +242,7 @@ class UTransformer_mhsa(SegmentationNetwork):
         self.segnest = models.unest3.UNest3(img_size=512, in_chans=1, 
                                         patch_size=4, num_levels=6, 
                                         embed_dims=(d,)*l, num_heads=(h,)*l, 
-                                        depths=(3,)*l, num_classes=2, 
+                                        depths=(2,)*l, num_classes=2, 
                                         mlp_ratio=4.).float()
 
         self.upscale_logits_ops = []
