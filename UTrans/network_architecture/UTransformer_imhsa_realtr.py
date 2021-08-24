23e9a3bcc1cb7d533f8a9365e0f50aebedf0950a
Myyyr <loic.themyr@gmail.com>
2021-08-24 17:02:41 +0200

    update nnunet with tensoboard, memory, parameters

--- UTrans/network_architecture/UTransformer_imhsa_realtr.py
+++ UTrans/network_architecture/UTransformer_imhsa_realtr.py
@@ -24,8 +24,89 @@ import torch.nn.functional
 
 from einops import rearrange, repeat
 import math
+from torch import einsum
+import torch.nn.functional as F
+
+from einops.layers.torch import Rearrange
 
 import revtorch as rv  
+
+# helpers
+
+def pair(t):
+    return t if isinstance(t, tuple) else (t, t)
+
+# classes
+
+class PreNorm(nn.Module):
+    def __init__(self, dim, fn):
+        super().__init__()
+        self.norm = nn.LayerNorm(dim)
+        self.fn = fn
+    def forward(self, x, **kwargs):
+        return self.fn(self.norm(x), **kwargs)
+
+class FeedForward(nn.Module):
+    def __init__(self, dim, hidden_dim, dropout = 0.):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(dim, hidden_dim),
+            nn.GELU(),
+            nn.Dropout(dropout),
+            nn.Linear(hidden_dim, dim),
+            nn.Dropout(dropout)
+        )
+    def forward(self, x):
+        return self.net(x)
+
+class Attention(nn.Module):
+    def __init__(self, dim, heads = 8, dim_head = 64, dropout = 0.):
+        super().__init__()
+        inner_dim = dim_head *  heads
+        project_out = not (heads == 1 and dim_head == dim)
+
+        self.heads = heads
+        self.scale = dim_head ** -0.5
+
+        self.attend = nn.Softmax(dim = -1)
+        self.to_qkv = nn.Linear(dim, inner_dim * 3, bias = False)
+
+        self.to_out = nn.Sequential(
+            nn.Linear(inner_dim, dim),
+            nn.Dropout(dropout)
+        ) if project_out else nn.Identity()
+
+    def forward(self, x):
+        b, n, _, h = *x.shape, self.heads
+        qkv = self.to_qkv(x).chunk(3, dim = -1)
+        q, k, v = map(lambda t: rearrange(t, 'b n (h d) -> b h n d', h = h), qkv)
+
+        dots = einsum('b h i d, b h j d -> b h i j', q, k) * self.scale
+
+        attn = self.attend(dots)
+
+        out = einsum('b h i j, b h j d -> b h i d', attn, v)
+        out = rearrange(out, 'b h n d -> b n (h d)')
+        return self.to_out(out)
+
+class Transformer(nn.Module):
+    def __init__(self, dim, depth, heads, dim_head, mlp_dim, dropout = 0.):
+        super().__init__()
+        self.layers = nn.ModuleList([])
+        for _ in range(depth):
+            attn = rv.ReversibleBlock(PreNorm(dim//2, Attention(dim//2, heads = heads, dim_head = dim_head, dropout = dropout)), 
+                                    PreNorm(dim//2, Attention(dim//2, heads = heads, dim_head = dim_head, dropout = dropout)), split_along_dim=2)
+            self.layers.append(attn)
+            ff = rv.ReversibleBlock(PreNorm(dim//2, FeedForward(dim//2, mlp_dim, dropout = dropout)),
+                                    PreNorm(dim//2, FeedForward(dim//2, mlp_dim, dropout = dropout)), split_along_dim=2)
+            self.layers.append(ff)
+
+        self.layers = rv.ReversibleSequence(self.layers)
+
+    def forward(self, x):
+        # print(x.shape)
+        x = self.layers(x)
+        return x
 
 
 class ConvDropoutNormNonlin(nn.Module):
@@ -169,7 +250,7 @@ class Upsample(nn.Module):
                                          align_corners=self.align_corners)
 
 
-class UTransformer_mhsa(SegmentationNetwork):
+class UTransformer_imhsa(SegmentationNetwork):
     DEFAULT_BATCH_SIZE_3D = 2
     DEFAULT_PATCH_SIZE_3D = (64, 192, 160)
     SPACING_FACTOR_BETWEEN_STAGES = 2
@@ -496,8 +577,9 @@ class MHSA(nn.Module):
         self.d = d
         self.n_heads = n_heads        
 
-        trans_layer = nn.TransformerEncoderLayer(d_model=self.d, nhead=self.n_heads)
-        self.self_trans = nn.TransformerEncoder(trans_layer, 6)
+        self.self_trans = Transformer(dim=self.d, depth=6, heads=self.n_heads, dim_head=self.d//self.n_heads, mlp_dim=2048, dropout = 0.1)
+        # trans_layer = nn.TransformerEncoderLayer(d_model=self.d, nhead=self.n_heads)
+        # self.self_trans = nn.TransformerEncoder(trans_layer, 6)
 
 
         self.pe = None 
