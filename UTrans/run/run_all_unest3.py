38cd9b418028cebdc1a1da2af0990fae0a875903
MYR <loic.themyr@gmail.com>
2021-08-25 09:59:22 +0200

    debug

--- UTrans/run/run_all_unest3.py
+++ UTrans/run/run_all_unest3.py
@@ -1,8 +1,8 @@
 import UTrans.run.nnunet_run_training as run
 
 g = '2'
-run.main(gpu=g, network='2d', network_trainer='nnUNetTrainerV2_utrans_unest3', task='062', fold=1, outpath='SEGNEST_V1', val=False, npz=True)
-run.main(gpu=g, network='2d', network_trainer='nnUNetTrainerV2_utrans_unest3', task='062', fold=1, outpath='SEGNEST_V1', val=True, npz=True)
+run.main(gpu=g, network='2d', network_trainer='nnUNetTrainerV2_utrans_unest3', task='062', fold=1, outpath='unestv3', val=False, npz=True)
+run.main(gpu=g, network='2d', network_trainer='nnUNetTrainerV2_utrans_unest3', task='062', fold=1, outpath='unestv3', val=True, npz=True)
 # run.main(gpu='1', network='2d', network_trainer='nnUNetTrainerV2_utrans_mhsa', task='062', fold=4, outpath='MHSA_v2', val=False, npz=True)
 # run.main(gpu='1', network='2d', network_trainer='nnUNetTrainerV2_utrans_mhsa', task='062', fold=4, outpath='MHSA_v2', val=True, npz=True)
 # run.main(gpu='1', network='2d', network_trainer='nnUNetTrainerV2_utrans_mhsa', task='062', fold=2, outpath='MHSA_v2', val=False, npz=True)
