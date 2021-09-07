b2fb70cab4e4eedd30506f656976cfe592e83eba
Myyyr <loic.themyr@gmail.com>
2021-09-07 15:09:34 +0200

    iunestextrabig

--- UTrans/run/run_all_iunest3ExtraBig.py
+++ UTrans/run/run_all_iunest3ExtraBig.py
@@ -1,6 +1,6 @@
 import UTrans.run.nnunet_run_training as run
 
-g = '3'
+g = '0'
 run.main(gpu=g, network='2d', network_trainer='nnUNetTrainerV2_utrans_iunest3ExtraBig', task='062', fold=1, outpath='iunestv3ExtraBig', val=False, npz=True)
 run.main(gpu=g, network='2d', network_trainer='nnUNetTrainerV2_utrans_iunest3ExtraBig', task='062', fold=1, outpath='iunestv3ExtraBig', val=True,  npz=True)
 # run.main(gpu='1', network='2d', network_trainer='nnUNetTrainerV2_utrans_mhsa', task='062', fold=4, outpath='MHSA_v2', val=False, npz=True)
