3ef519180521731a3b7b34c3944c0419962ed685
MYR <loic.themyr@gmail.com>
2021-09-14 15:34:18 +0200

    cotr 3d - debug

--- UTrans/training/network_training/nnUNetTrainerV2_ResTrans.py
+++ UTrans/training/network_training/nnUNetTrainerV2_ResTrans.py
@@ -38,6 +38,9 @@ class nnUNetTrainerV2_ResTrans(nnUNetTrainer):
                  unpack_data=True, deterministic=True, fp16=False):
         super().__init__(plans_file, fold, output_folder, dataset_directory, batch_dice, stage, unpack_data,
                          deterministic, fp16)
+
+        print("!!! OK !!!")
+        exit(0)
         self.max_num_epochs = 1000
         self.initial_lr = 1e-2
         self.deep_supervision_scales = None
