69106f5ce9b65ad746844d8331d0c20de6345e54
MYR <loic.themyr@gmail.com>
2021-09-14 15:43:38 +0200

    cotr 3d - debug

--- UTrans/run/run_training.py
+++ UTrans/run/run_training.py
@@ -112,18 +112,23 @@ def main(gpu, network, network_trainer, task, fold, outpath, val, npz, c=False, 
     # trainer_class = get_default_configuration(outpath, network, task, network_trainer, plans_identifier, \
     #                                           search_in=(UTrans.__path__[0], "training", "network_training"), \
     #                                           base_module='UTrans.training.network_training')
+    
+    print("Here its ok 1")
+    exit(0)
     plans_file, output_folder_name, dataset_directory, batch_dice, stage, \
     trainer_class = get_default_configuration(outpath, network, task, network_trainer, plans_identifier, \
                                               search_in="UTrans.training.network_training", \
                                               base_module='UTrans.training.network_training')
 
     # print("---->",output_folder_name)
+    print("Here its ok 2")
+    exit(0)
     trainer = trainer_class(plans_file, fold, norm_cfg, activation_cfg, output_folder=output_folder_name, dataset_directory=dataset_directory,
                             batch_dice=batch_dice, stage=stage, unpack_data=decompress_data,
                             deterministic=deterministic,
                             fp16=run_mixed_precision)
 
-    print("Here its ok")
+    print("Here its ok 3")
     exit(0)
     # print(trainer_class)
     # trainer = trainer_class(plans_file, fold, output_folder=output_folder_name, dataset_directory=dataset_directory, batch_dice=batch_dice, stage=stage,
