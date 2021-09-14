8aab58db3a6fd34aa63c21809430738f990ea34b
MYR <loic.themyr@gmail.com>
2021-09-14 09:54:04 +0200

    segenst 3d - debug

--- UTrans/training/network_training/nnUNetTrainerV2_ResTrans.py
+++ UTrans/training/network_training/nnUNetTrainerV2_ResTrans.py
@@ -4,12 +4,12 @@ from typing import Tuple
 import numpy as np
 import torch
 import shutil
+from nnunet.training.data_augmentation.data_augmentation_moreDA import get_moreDA_augmentation
 from nnunet.training.loss_functions.deep_supervision import MultipleOutputLoss2
 from nnunet.utilities.to_torch import maybe_to_torch, to_cuda
-from nnunet.training.data_augmentation.data_augmentation_moreDA import get_moreDA_augmentation
 from nnunet.network_architecture.generic_UNet import Generic_UNet
 from nnunet.network_architecture.initialization import InitWeights_He
-from UTrans.network_architecture.neural_network import SegmentationNetwork
+from nnunet.network_architecture.neural_network import SegmentationNetwork
 from nnunet.training.data_augmentation.default_data_augmentation import default_2D_augmentation_params, \
     get_patch_size, default_3D_augmentation_params
 from nnunet.training.dataloading.dataset_loading import unpack_dataset
@@ -21,8 +21,8 @@ from torch.cuda.amp import autocast
 from nnunet.training.learning_rate.poly_lr import poly_lr
 from batchgenerators.utilities.file_and_folder_operations import *
 
-from nnunet.network_architecture.generic_modular_UNet import get_default_network_config
 from UTrans.network_architecture.ResTranUnet import ResTranUnet
+# from nnunet.network_architecture.generic_modular_UNet import get_default_network_config
 
 class nnUNetTrainerV2_ResTrans(nnUNetTrainer):
 
