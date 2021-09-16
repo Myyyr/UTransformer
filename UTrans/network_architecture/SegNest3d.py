657b5cbd3449a583bc7983a67946953eae6c0268
Myyyr <loic.themyr@gmail.com>
2021-09-16 11:13:02 +0200

    setr3d - debug

--- UTrans/network_architecture/SegNest3d.py
+++ UTrans/network_architecture/SegNest3d.py
@@ -240,7 +240,7 @@ class UTransformer_mhsa(SegmentationNetwork):
         d = h*64
         lvl = 3
         self.segnest = models.segnest3D.SegNest3d(img_size=(48, 192, 192), in_chans=1, 
-                      patch_size=(2,8,8), num_levels=lvl, 
+                      patch_size=(1,4,4), num_levels=lvl, 
                       embed_dims=(d,)*lvl, num_heads=(h,)*lvl, 
                       depths=(3,)*lvl, num_classes=2, 
                       mlp_ratio=4.).float()
