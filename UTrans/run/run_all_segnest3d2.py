63c08d961113a11fbaef16f73900f579319d021b
Myyyr <loic.themyr@gmail.com>
2021-09-16 10:50:16 +0200

    setr3d - debug

--- UTrans/run/run_all_segnest3d2.py
+++ UTrans/run/run_all_segnest3d2.py
@@ -1,8 +1,8 @@
 import run_training as run
 
 g = '2'
-run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2_Setr3d', task='062', fold=1, outpath='SEGNEST3D', val=False, npz=True)
-run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2_Setr3d', task='062', fold=1, outpath='SEGNEST3D', val=True, npz=True)
+run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2_segnest3d', task='062', fold=1, outpath='SEGNEST3D', val=False, npz=True)
+run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2_segnest3d', task='062', fold=1, outpath='SEGNEST3D', val=True, npz=True)
 # run.main(gpu='1', network='2d', network_trainer='nnUNetTrainerV2_utrans_mhsa', task='062', fold=4, outpath='MHSA_v2', val=False, npz=True)
 # run.main(gpu='1', network='2d', network_trainer='nnUNetTrainerV2_utrans_mhsa', task='062', fold=4, outpath='MHSA_v2', val=True, npz=True)
 # run.main(gpu='1', network='2d', network_trainer='nnUNetTrainerV2_utrans_mhsa', task='062', fold=2, outpath='MHSA_v2', val=False, npz=True)
