679486def03518e1e39dd9ea7c4b6134cfb52c26
Myyyr <loic.themyr@gmail.com>
2021-09-30 11:59:03 +0200

    cotr synapse

--- UTrans/run/synapse_run_all_unet.py
+++ UTrans/run/synapse_run_all_unet.py
@@ -1,7 +1,7 @@
 import UTrans.run.nnunet_run_training as run
 
 g='2'
-run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2', task='17', fold=1, outpath='SynpaseUNET_v2', val=False, npz=True)
+# run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2', task='17', fold=1, outpath='SynpaseUNET_v2', val=False, npz=True)
 run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2', task='17', fold=1, outpath='SynpaseUNET_v2', val=True, npz=True)
 # run.main(gpu='0', network='2d', network_trainer='nnUNetTrainerV2', task='062', fold=4, outpath='UNET_v2', val=False, npz=True)
 # run.main(gpu='0', network='2d', network_trainer='nnUNetTrainerV2', task='062', fold=4, outpath='UNET_v2', val=True, npz=True)
