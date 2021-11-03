e6cf7ca7532aaffe654e5430a1a13aa7ba034750
Myyyr <loic.themyr@gmail.com>
2021-11-03 17:02:30 +0100

    medical gt nnformer

--- UTrans/training/network_training/nnUNetTrainerV2_nnFormer_good.py
+++ UTrans/training/network_training/nnUNetTrainerV2_nnFormer_good.py
@@ -69,7 +69,7 @@ class nnUNetTrainerV2_nnFormerGood(nnUNetTrainer):
 
             if force_load_plans or (self.plans is None):
                 self.load_plans_file()
-               self.plans['plans_per_stage'][1]['patch_size'] = [64,128,128]
+                self.plans['plans_per_stage'][1]['patch_size'] = [64,128,128]
                 print("Patch size is %s" % self.plans['plans_per_stage'][1]['patch_size'])
                 # exit(0)
                 if self.norm_cfg=='BN':
