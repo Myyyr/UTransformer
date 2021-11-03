ba0388292ffd542d8bcf9d3606a0a95bdb97c400
Myyyr <loic.themyr@gmail.com>
2021-11-03 16:09:00 +0100

    medical gt nnformer

--- UTrans/training/network_training/nnUNetTrainerV2_nnFormerGT10.py
+++ UTrans/training/network_training/nnUNetTrainerV2_nnFormerGT10.py
@@ -161,7 +161,7 @@ class nnUNetTrainerV2_nnFormerGT10(nnUNetTrainer):
                                     self.conv_per_stage, 2, conv_op, norm_op, norm_op_kwargs, dropout_op,
                                     dropout_op_kwargs,
                                     net_nonlin, net_nonlin_kwargs, True, False, lambda x: x, InitWeights_He(1e-2),
-                                    self.net_num_pool_op_kernel_sizes, self.net_conv_kernel_sizes, False, True, True, , gt_num=10)
+                                    self.net_num_pool_op_kernel_sizes, self.net_conv_kernel_sizes, False, True, True,gt_num=10)
 
         total = sum([param.nelement() for param in self.network.parameters()])
         print('  + Number of Network Params: %.2f(e6)' % (total / 1e6))
