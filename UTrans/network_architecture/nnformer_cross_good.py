b448487d63f3cef98c52b411ffad240208ddf01a
MYR <loic.themyr@gmail.com>
2021-11-11 00:10:12 +0100

    nnformer cross

--- UTrans/network_architecture/nnformer_cross_good.py
+++ UTrans/network_architecture/nnformer_cross_good.py
@@ -1304,9 +1304,9 @@ class encoder(nn.Module):
                                                            drop_path=dpr[sum(depths[:(self.num_layers-1-i_layer)]):sum(depths[:(self.num_layers-1-i_layer) + 1])],
                                                            norm_layer=norm_layer,
                                                            residual_patch_expand=residual_patch_expand)
-            patch_expand = Patch_Expanding(input_resolution=(patches_resolution[0] // (2 ** (self.num_layers-1-i_layer)),
-                                                         patches_resolution[1] // (2 ** (self.num_layers-1-i_layer))),
-                                       dim=int(embed_dim * 2 ** (self.num_layers-1-i_layer + 1)),
+            # patch_expand = Patch_Expanding(input_resolution=(patches_resolution[0] // (2 ** (self.num_layers-1-i_layer)),
+            #                                              patches_resolution[1] // (2 ** (self.num_layers-1-i_layer))),
+            patch_expand = Patch_Expanding(dim=int(embed_dim * 2 ** (self.num_layers-1-i_layer + 1)),
                                        dim_scale=2)
             self.layers.append(layer)
             self.concat_back_dim.append(concat_linear)
