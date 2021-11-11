d3dd8199ca200c53ba03702b7970d4409b5a9644
MYR <loic.themyr@gmail.com>
2021-11-11 11:37:02 +0100

    nnformer gt 5 mep

--- UTrans/run/run_all_nnformergt5gv2.py
+++ UTrans/run/run_all_nnformergt5gv2.py
@@ -1,8 +1,8 @@
 import run_training as run
 
 g = '0'
-run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2_nnFormerGT5gv2', task='017', fold=1, outpath='NNFORMERGT5gv2', val=False, npz=True)
-run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2_nnFormerGT5gv2', task='017', fold=1, outpath='NNFORMERGT5gv2', val=True,  npz=True)
+run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2_nnFormerGT5gv2', task='017', fold=1, outpath='NNFORMERGT5gv2Mep', val=False, npz=True)
+run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2_nnFormerGT5gv2', task='017', fold=1, outpath='NNFORMERGT5gv2Mep', val=True,  npz=True)
 # run.main(gpu='1', network='2d', network_trainer='nnUNetTrainerV2_utrans_mhsa', task='062', fold=4, outpath='MHSA_v2', val=False, npz=True)
 # run.main(gpu='1', network='2d', network_trainer='nnUNetTrainerV2_utrans_mhsa', task='062', fold=4, outpath='MHSA_v2', val=True, npz=True)
 # run.main(gpu='1', network='2d', network_trainer='nnUNetTrainerV2_utrans_mhsa', task='062', fold=2, outpath='MHSA_v2', val=False, npz=True)
