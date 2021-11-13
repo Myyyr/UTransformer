8cbc0218a3195bc94f35f6d87d207ee6c0b78e9a
MYR <loic.themyr@gmail.com>
2021-11-13 11:12:44 +0100

    nnformer continue

---
+++ UTrans/run/run_continue_nnformer_gt10.py
@@ -0,0 +1,14 @@
+import UTrans.run.nnunet_run_training as run
+import os
+BASE_DIR="/local/DEEPLEARNING/nnUNetData/nnUNet_trained_models/nnUNet/3d_fullres_nnUNetPlansv2.1/Task017_BCV/"
+MODEL_PATH = "fold_1/model_final_checkpoint.model"
+
+
+mod = "NNFORMERCROSS_IN_LeakyReLU/"
+for i in range(2,7):
+	pth = os.path.join(BASE_DIR,mod,MODEL_PATH)
+	print(pth)
+	# exit(0)
+	run.main(gpu='1', network='3d', network_trainer='nnUNetTrainerV2_nnFormerCrossGood', task='017', fold=1, outpath='NNFORMERCROSS_c'+str(i), val=False, npz=True, c=False, ep=50, lr=2e-05, pretrained_weights=pth)
+	run.main(gpu='1', network='3d', network_trainer='nnUNetTrainerV2_nnFormerCrossGood', task='017', fold=1, outpath='NNFORMERCROSS_c'+str(i), val=True,  npz=True, c=False, ep=50, lr=2e-05 )
+	mod = "NNFORMERCROSS_c"+str(i)+"_IN_LeakyReLU/"
