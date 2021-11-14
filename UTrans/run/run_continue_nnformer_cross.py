a1114df678b843001cc3e83616a5d90bf06dc1a1
MYR <loic.themyr@gmail.com>
2021-11-14 09:47:39 +0100

    nnformer continue

--- UTrans/run/run_continue_nnformer_cross.py
+++ UTrans/run/run_continue_nnformer_cross.py
@@ -1,14 +1,19 @@
 import UTrans.run.nnunet_run_training as run
 import os
+import torch
+
 BASE_DIR="/local/DEEPLEARNING/nnUNetData/nnUNet_trained_models/nnUNet/3d_fullres_nnUNetPlansv2.1/Task017_BCV/"
 MODEL_PATH = "fold_1/model_final_checkpoint.model"
 
 
-mod = "NNFORMERCROSS_IN_LeakyReLU/"
-for i in range(1,10):
+mod = "NNFORMERCROSS_c2_IN_LeakyReLU/"
+for i in range(2,10):
 	pth = os.path.join(BASE_DIR,mod,MODEL_PATH)
 	print(pth)
 	# exit(0)
 	run.main(gpu='1', network='3d_fullres', network_trainer='nnUNetTrainerV2_nnFormerCrossGood', task='017', fold=1, outpath='NNFORMERCROSS_c'+str(i), val=False, npz=True, c=False, ep=50, lr=2e-05, pretrained_weights=pth)
+	torch.cuda.empty_cache()
 	run.main(gpu='1', network='3d_fullres', network_trainer='nnUNetTrainerV2_nnFormerCrossGood', task='017', fold=1, outpath='NNFORMERCROSS_c'+str(i), val=True,  npz=True, c=False, ep=50, lr=2e-05 )
 	mod = "NNFORMERCROSS_c"+str(i)+"_IN_LeakyReLU/"
+	torch.cuda.empty_cache()
+	
