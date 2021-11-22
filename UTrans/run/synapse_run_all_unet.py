8c813e669587db069178dba7833af3505efd726e
MYR <loic.themyr@gmail.com>
2021-11-22 16:49:24 +0100

    sync

--- UTrans/run/synapse_run_all_unet.py
+++ UTrans/run/synapse_run_all_unet.py
@@ -1,8 +1,9 @@
-import UTrans.run.nnunet_run_training as run
+# import UTrans.run.nnunet_run_training as run
+import run_training as run
 
-g='2'
-# run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2', task='17', fold=1, outpath='SynpaseUNET_v2', val=False, npz=True)
-run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2', task='17', fold=1, outpath='SynpaseUNET_v2', val=True, npz=True)
+g='0'
+run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2', task='17', fold=1, outpath='SynapseUNET_v2', val=False, npz=True)
+run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2', task='17', fold=1, outpath='SynapseUNET_v2', val=True, npz=True)
 # run.main(gpu='0', network='2d', network_trainer='nnUNetTrainerV2', task='062', fold=4, outpath='UNET_v2', val=False, npz=True)
 # run.main(gpu='0', network='2d', network_trainer='nnUNetTrainerV2', task='062', fold=4, outpath='UNET_v2', val=True, npz=True)
 # run.main(gpu='0', network='2d', network_trainer='nnUNetTrainerV2', task='062', fold=2, outpath='UNET_v2', val=False, npz=True)
