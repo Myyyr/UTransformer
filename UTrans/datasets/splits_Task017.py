dbbc640077fea12e7d3e17870f46cf93b69a8c68
Myyyr <Myyyr>
2022-02-08 12:12:14 +0100

    splits

---
+++ UTrans/datasets/splits_Task017.py
@@ -0,0 +1,7 @@
+import pickle as pkl
+from collections import OrderedDict
+
+
+l=[OrderedDict([('train', ['bcv_1', 'bcv_2', 'bcv_3', 'bcv_4', 'bcv_5', 'bcv_6', 'bcv_7', 'bcv_8', 'bcv_9', 'bcv_10', 'bcv_21', 'bcv_22', 'bcv_23', 'bcv_24', 'bcv_25', 'bcv_26', 'bcv_27', 'bcv_28', 'bcv_29', 'bcv_30', 'bcv_31']), ('val', ['bcv_32', 'bcv_33', 'bcv_34', 'bcv_35', 'bcv_36', 'bcv_37', 'bcv_38', 'bcv_39', 'bcv_40'])]), OrderedDict([('train', ['bcv_1','bcv_2','bcv_3','bcv_4','bcv_5','bcv_6','bcv_7','bcv_8','bcv_9','bcv_10','bcv_21','bcv_22','bcv_32','bcv_33','bcv_34', 'bcv_35', 'bcv_36', 'bcv_37', 'bcv_38', 'bcv_39', 'bcv_40']), ('val',  ['bcv_23', 'bcv_24', 'bcv_25', 'bcv_26', 'bcv_27', 'bcv_28', 'bcv_29', 'bcv_30', 'bcv_31'])]), OrderedDict([('train', ['bcv_1', 'bcv_2', 'bcv_3', 'bcv_23', 'bcv_24', 'bcv_25', 'bcv_26', 'bcv_27', 'bcv_28', 'bcv_29', 'bcv_30', 'bcv_31', 'bcv_32', 'bcv_33', 'bcv_34', 'bcv_35', 'bcv_36', 'bcv_37', 'bcv_38', 'bcv_39', 'bcv_40']), ('val', ['bcv_4', 'bcv_5', 'bcv_6', 'bcv_7', 'bcv_8', 'bcv_9', 'bcv_10', 'bcv_21', 'bcv_22'])]), OrderedDict([('train', ['bcv_5', 'bcv_6', 'bcv_7', 'bcv_8', 'bcv_9', 'bcv_10', 'bcv_21', 'bcv_24', 'bcv_25', 'bcv_26', 'bcv_27', 'bcv_28', 'bcv_29', 'bcv_30', 'bcv_33', 'bcv_34', 'bcv_35', 'bcv_36', 'bcv_37', 'bcv_38', 'bcv_39']), ('val', ['bcv_1', 'bcv_2', 'bcv_3', 'bcv_4', 'bcv_22', 'bcv_23', 'bcv_31', 'bcv_32', 'bcv_40'])])]
+with open("splits_Task017.pkl", "wb") as f:
+	pkl.dump(l,f)
