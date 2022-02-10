505884f46d376628824115197b610ead963729c9
MYR <loic.themyr@gmail.com>
2022-02-10 09:41:18 +0100

    extended  - pos

--- UTrans/res.py
+++ UTrans/res.py
@@ -15,7 +15,7 @@ def main():
 	path = os.path.join(path, args.model, "fold_"+args.fold, "validation_raw_postprocessed/summary.json")
 
 	with open(path, 'r') as f:
-		data=json.load(f)
+		data=json.load(f)['results']
 		for key, value in data.items():
 			print("Key:", key)
 
