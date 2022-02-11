95712c0f298fcfc6fdbbf3fe1bbe00d15fe31279
MYR <loic.themyr@gmail.com>
2022-02-11 10:07:43 +0100

    extended  - pos

--- UTrans/run/run_all_cotr.py
+++ UTrans/run/run_all_cotr.py
@@ -1,7 +1,7 @@
 import run_training as run
 
 g = '2'
-#run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2_CoTR', task='017', fold=0, outpath='COTRaf', val=False, npz=True, na=True)
+run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2_CoTR', task='017', fold=0, outpath='COTRaf', val=False, npz=True, na=True)
 run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2_CoTR', task='017', fold=0, outpath='COTRaf', val=True, npz=True, na=True)
 run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2_CoTR', task='017', fold=1, outpath='COTRaf', val=False, npz=True, na=True)
 run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2_CoTR', task='017', fold=1, outpath='COTRaf', val=True, npz=True, na=True)
