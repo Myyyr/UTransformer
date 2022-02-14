cb2fea78a144a3c31cce5158d33d63da21531a1b
MYR <loic.themyr@gmail.com>
2022-02-14 22:13:37 +0100

    extended  - pos

--- UTrans/training/network_training/brats_nnUNetTrainerV2_nnFormer.py
+++ UTrans/training/network_training/brats_nnUNetTrainerV2_nnFormer.py
@@ -1,9 +1,23 @@
+#    Copyright 2020 Division of Medical Image Computing, German Cancer Research Center (DKFZ), Heidelberg, Germany
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
+
 from collections import OrderedDict
 from typing import Tuple
 
 import numpy as np
 import torch
-import shutil
 from nnunet.training.data_augmentation.data_augmentation_moreDA import get_moreDA_augmentation
 from nnunet.training.loss_functions.deep_supervision import MultipleOutputLoss2
 from nnunet.utilities.to_torch import maybe_to_torch, to_cuda
@@ -21,45 +35,31 @@ from torch.cuda.amp import autocast
 from nnunet.training.learning_rate.poly_lr import poly_lr
 from batchgenerators.utilities.file_and_folder_operations import *
 
+
 from UTrans.network_architecture.nnformer import swintransformer
-# from nnunet.network_architecture.generic_modular_UNet import get_default_network_config
-# python run_training.py -network='2d' -network_trainer=nnUNetTrainerV2_utrans_mhsa -task=062 -fold='all' -gpu=1 -outpath='MHSA'
-def get_n_params(model):
-    pp=0
-    for p in list(model.parameters()):
-        nn=1
-        for s in list(p.size()):
-            nn = nn*s
-        pp += nn
-    return pp
-class nnUNetTrainerV2_nnFormer(nnUNetTrainer):
+
+
+class brats_nnUNetTrainerV2_nnFormer(nnUNetTrainer):
+    """
+    Info for Fabian: same as internal nnUNetTrainerV2_2
+    """
 
     def __init__(self, plans_file, fold, output_folder=None, dataset_directory=None, batch_dice=True, stage=None,
                  unpack_data=True, deterministic=True, fp16=False):
         super().__init__(plans_file, fold, output_folder, dataset_directory, batch_dice, stage, unpack_data,
                          deterministic, fp16)
-
-        # print("!!! OK !!!")
-        # exit(0)
         self.max_num_epochs = 1000
         self.initial_lr = 1e-2
         self.deep_supervision_scales = None
         self.ds_loss_weights = None
 
-        # self.norm_cfg = norm_cfg
-        # self.activation_cfg = activation_cfg
-
         self.pin_memory = True
-
-        self.save_best_checkpoint = True
-
 
     def initialize(self, training=True, force_load_plans=False):
         """
         - replaced get_default_augmentation with get_moreDA_augmentation
         - enforce to only run this code once
         - loss function wrapper for deep supervision
-
         :param training:
         :param force_load_plans:
         :return:
@@ -69,14 +69,6 @@ class nnUNetTrainerV2_nnFormer(nnUNetTrainer):
 
             if force_load_plans or (self.plans is None):
                 self.load_plans_file()
-                self.plans['plans_per_stage'][1]['patch_size'] = [64,128,128]
-                print("Patch size is %s" % self.plans['plans_per_stage'][1]['patch_size'])
-                # exit(0)
-                # if self.norm_cfg=='BN':
-                    # self.plans['plans_per_stage'][1]['batch_size'] = 8
-
-                # self.plans['plans_per_stage'][1]['batch_size'] = 1   #Debug
-                print("Batch size is %s" % self.plans['plans_per_stage'][1]['batch_size'])
 
             self.process_plans(self.plans)
 
@@ -118,7 +110,8 @@ class nnUNetTrainerV2_nnFormer(nnUNetTrainer):
                         'patch_size_for_spatialtransform'],
                     self.data_aug_params,
                     deep_supervision_scales=self.deep_supervision_scales,
-                    pin_memory=self.pin_memory
+                    pin_memory=self.pin_memory,
+                    use_nondetMultiThreadedAugmenter=False
                 )
                 self.print_to_log_file("TRAINING KEYS:\n %s" % (str(self.dataset_tr.keys())),
                                        also_print_to_console=False)
@@ -129,7 +122,7 @@ class nnUNetTrainerV2_nnFormer(nnUNetTrainer):
 
             self.initialize_network()
             self.initialize_optimizer_and_scheduler()
-            print(type(self.network))
+
             assert isinstance(self.network, (SegmentationNetwork, nn.DataParallel))
         else:
             self.print_to_log_file('self.was_initialized is True, not running self.initialize again')
@@ -142,33 +135,29 @@ class nnUNetTrainerV2_nnFormer(nnUNetTrainer):
         - self.lr_scheduler = None because we do poly_lr
         - deep supervision = True
         - i am sure I forgot something here
-
         Known issue: forgot to set neg_slope=0 in InitWeights_He; should not make a difference though
         :return:
         """
-        # self.network = ResTranUnet(norm_cfg=self.norm_cfg, activation_cfg=self.activation_cfg, img_size=self.plans['plans_per_stage'][1]['patch_size'],
-        #                          num_classes=self.num_classes, weight_std=False, deep_supervision=True).cuda()
-        conv_op = nn.Conv3d
-        dropout_op = nn.Dropout3d
-        norm_op = nn.InstanceNorm3d
+        if self.threeD:
+            conv_op = nn.Conv3d
+            dropout_op = nn.Dropout3d
+            norm_op = nn.InstanceNorm3d
+
+        else:
+            conv_op = nn.Conv2d
+            dropout_op = nn.Dropout2d
+            norm_op = nn.InstanceNorm2d
+
         norm_op_kwargs = {'eps': 1e-5, 'affine': True}
         dropout_op_kwargs = {'p': 0, 'inplace': True}
         net_nonlin = nn.LeakyReLU
         net_nonlin_kwargs = {'negative_slope': 1e-2, 'inplace': True}
-        
         self.network = swintransformer(self.num_input_channels, self.base_num_features, self.num_classes,
                                     len(self.net_num_pool_op_kernel_sizes),
                                     self.conv_per_stage, 2, conv_op, norm_op, norm_op_kwargs, dropout_op,
                                     dropout_op_kwargs,
                                     net_nonlin, net_nonlin_kwargs, True, False, lambda x: x, InitWeights_He(1e-2),
                                     self.net_num_pool_op_kernel_sizes, self.net_conv_kernel_sizes, False, True, True)
-
-        total = sum([param.nelement() for param in self.network.parameters()])
-        print('  + Number of Network Params: %.2f(e6)' % (total / 1e6))
-
-        print("####\n#### MODEL PARAMS :{}\n####".format(get_n_params(self.network)))
-        # exit(0)
-
         if torch.cuda.is_available():
             self.network.cuda()
         self.network.inference_apply_nonlin = softmax_helper
@@ -177,7 +166,6 @@ class nnUNetTrainerV2_nnFormer(nnUNetTrainer):
         assert self.network is not None, "self.initialize_network must be called first"
         self.optimizer = torch.optim.SGD(self.network.parameters(), self.initial_lr, weight_decay=self.weight_decay,
                                          momentum=0.99, nesterov=True)
-
         self.lr_scheduler = None
 
     def run_online_evaluation(self, output, target):
@@ -195,7 +183,7 @@ class nnUNetTrainerV2_nnFormer(nnUNetTrainer):
     def validate(self, do_mirroring: bool = True, use_sliding_window: bool = True,
                  step_size: float = 0.5, save_softmax: bool = True, use_gaussian: bool = True, overwrite: bool = True,
                  validation_folder_name: str = 'validation_raw', debug: bool = False, all_in_gpu: bool = False,
-                 segmentation_export_kwargs: dict = None):
+                 segmentation_export_kwargs: dict = None, run_postprocessing_on_folds: bool = True):
         """
         We need to wrap this because we need to enforce self.network.do_ds = False for prediction
         """
@@ -204,7 +192,8 @@ class nnUNetTrainerV2_nnFormer(nnUNetTrainer):
         ret = super().validate(do_mirroring=do_mirroring, use_sliding_window=use_sliding_window, step_size=step_size,
                                save_softmax=save_softmax, use_gaussian=use_gaussian,
                                overwrite=overwrite, validation_folder_name=validation_folder_name, debug=debug,
-                               all_in_gpu=all_in_gpu, segmentation_export_kwargs=segmentation_export_kwargs)
+                               all_in_gpu=all_in_gpu, segmentation_export_kwargs=segmentation_export_kwargs,
+                               run_postprocessing_on_folds=run_postprocessing_on_folds)
 
         self.network.do_ds = ds
         return ret
@@ -235,7 +224,6 @@ class nnUNetTrainerV2_nnFormer(nnUNetTrainer):
     def run_iteration(self, data_generator, do_backprop=True, run_online_evaluation=False):
         """
         gradient clipping improves training stability
-
         :param data_generator:
         :param do_backprop:
         :param run_online_evaluation:
@@ -256,8 +244,6 @@ class nnUNetTrainerV2_nnFormer(nnUNetTrainer):
 
         if self.fp16:
             with autocast():
-                # print(type(self.network))
-                # exit(0)
                 output = self.network(data)
                 del data
                 l = self.loss(output, target)
@@ -269,8 +255,6 @@ class nnUNetTrainerV2_nnFormer(nnUNetTrainer):
                 self.amp_grad_scaler.step(self.optimizer)
                 self.amp_grad_scaler.update()
         else:
-            # print(type(self.network))
-            # exit(0)
             output = self.network(data)
             del data
             l = self.loss(output, target)
@@ -289,23 +273,62 @@ class nnUNetTrainerV2_nnFormer(nnUNetTrainer):
 
     def do_split(self):
         """
-        Create a split and save it as splits_final.pkl file in the preprocessed data directory.
+        The default split is a 5 fold CV on all available training cases. nnU-Net will create a split (it is seeded,
+        so always the same) and save it as splits_final.pkl file in the preprocessed data directory.
+        Sometimes you may want to create your own split for various reasons. For this you will need to create your own
+        splits_final.pkl file. If this file is present, nnU-Net is going to use it and whatever splits are defined in
+        it. You can create as many splits in this file as you want. Note that if you define only 4 splits (fold 0-3)
+        and then set fold=4 when training (that would be the fifth split), nnU-Net will print a warning and proceed to
+        use a random 80:20 data split.
+        :return:
         """
-        # if the split file does not exist we need to create it
-        if not isfile(join(self.dataset_directory, "splits_final.pkl")):
-            shutil.copy('../../../data/splits_final.pkl', self.dataset_directory)
+        if self.fold == "all":
+            # if fold==all then we use all images for training and validation
+            tr_keys = val_keys = list(self.dataset.keys())
+        else:
+            splits_file = join(self.dataset_directory, "splits_final.pkl")
 
-        splits_file = join(self.dataset_directory, "splits_final.pkl")
-        splits = load_pickle(splits_file)
+            # if the split file does not exist we need to create it
+            if not isfile(splits_file):
+                self.print_to_log_file("Creating new 5-fold cross-validation split...")
+                splits = []
+                all_keys_sorted = np.sort(list(self.dataset.keys()))
+                kfold = KFold(n_splits=5, shuffle=True, random_state=12345)
+                for i, (train_idx, test_idx) in enumerate(kfold.split(all_keys_sorted)):
+                    train_keys = np.array(all_keys_sorted)[train_idx]
+                    test_keys = np.array(all_keys_sorted)[test_idx]
+                    splits.append(OrderedDict())
+                    splits[-1]['train'] = train_keys
+                    splits[-1]['val'] = test_keys
+                save_pickle(splits, splits_file)
 
-        tr_keys = splits[0]['train']
-        val_keys = splits[0]['val']
+            else:
+                self.print_to_log_file("Using splits from existing split file:", splits_file)
+                splits = load_pickle(splits_file)
+                self.print_to_log_file("The split file contains %d splits." % len(splits))
+
+            self.print_to_log_file("Desired fold for training: %d" % self.fold)
+            if self.fold < len(splits):
+                tr_keys = splits[self.fold]['train']
+                val_keys = splits[self.fold]['val']
+                self.print_to_log_file("This split has %d training and %d validation cases."
+                                       % (len(tr_keys), len(val_keys)))
+            else:
+                self.print_to_log_file("INFO: You requested fold %d for training but splits "
+                                       "contain only %d folds. I am now creating a "
+                                       "random (but seeded) 80:20 split!" % (self.fold, len(splits)))
+                # if we request a fold that is not in the split file, create a random 80:20 split
+                rnd = np.random.RandomState(seed=12345 + self.fold)
+                keys = np.sort(list(self.dataset.keys()))
+                idx_tr = rnd.choice(len(keys), int(len(keys) * 0.8), replace=False)
+                idx_val = [i for i in range(len(keys)) if i not in idx_tr]
+                tr_keys = [keys[i] for i in idx_tr]
+                val_keys = [keys[i] for i in idx_val]
+                self.print_to_log_file("This random 80:20 split has %d training and %d validation cases."
+                                       % (len(tr_keys), len(val_keys)))
 
         tr_keys.sort()
         val_keys.sort()
-        print("Current train-val split is ...")
-        print('Training set is %s' % tr_keys)
-        print('Validation set is %s \n' % val_keys)
         self.dataset_tr = OrderedDict()
         for i in tr_keys:
             self.dataset_tr[i] = self.dataset[i]
@@ -318,14 +341,12 @@ class nnUNetTrainerV2_nnFormer(nnUNetTrainer):
         - we increase roation angle from [-15, 15] to [-30, 30]
         - scale range is now (0.7, 1.4), was (0.85, 1.25)
         - we don't do elastic deformation anymore
-
         :return:
         """
 
-        self.downsampe_scales = [[1, 2, 2], [2, 2, 2], [2, 2, 2], [2, 2, 2]]
         self.deep_supervision_scales = [[1, 1, 1]] + list(list(i) for i in 1 / np.cumprod(
-            np.vstack(self.downsampe_scales), axis=0))[:-1]
-            
+            np.vstack(self.net_num_pool_op_kernel_sizes), axis=0))[:-1]
+
         if self.threeD:
             self.data_aug_params = default_3D_augmentation_params
             self.data_aug_params['rotation_x'] = (-30. / 360 * 2. * np.pi, 30. / 360 * 2. * np.pi)
@@ -353,28 +374,24 @@ class nnUNetTrainerV2_nnFormer(nnUNetTrainer):
                                                              self.data_aug_params['rotation_z'],
                                                              self.data_aug_params['scale_range'])
             self.basic_generator_patch_size = np.array([self.patch_size[0]] + list(self.basic_generator_patch_size))
-            patch_size_for_spatialtransform = self.patch_size[1:]
         else:
             self.basic_generator_patch_size = get_patch_size(self.patch_size, self.data_aug_params['rotation_x'],
                                                              self.data_aug_params['rotation_y'],
                                                              self.data_aug_params['rotation_z'],
                                                              self.data_aug_params['scale_range'])
-            patch_size_for_spatialtransform = self.patch_size
 
         self.data_aug_params["scale_range"] = (0.7, 1.4)
         self.data_aug_params["do_elastic"] = False
         self.data_aug_params['selected_seg_channels'] = [0]
-        self.data_aug_params['patch_size_for_spatialtransform'] = patch_size_for_spatialtransform
+        self.data_aug_params['patch_size_for_spatialtransform'] = self.patch_size
 
         self.data_aug_params["num_cached_per_thread"] = 2
 
     def maybe_update_lr(self, epoch=None):
         """
         if epoch is not None we overwrite epoch. Else we use epoch = self.epoch + 1
-
         (maybe_update_lr is called in on_epoch_end which is called before epoch is incremented.
         herefore we need to do +1 here)
-
         :param epoch:
         :return:
         """
@@ -409,7 +426,6 @@ class nnUNetTrainerV2_nnFormer(nnUNetTrainer):
         """
         if we run with -c then we need to set the correct lr for the first epoch, otherwise it will run the first
         continued epoch with self.initial_lr
-
         we also need to make sure deep supervision in the network is enabled for training, thus the wrapper
         :return:
         """
