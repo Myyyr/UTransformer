1ffd1070da478bddeef6b9d82b04ab10120a08d1
MYR <loic.themyr@gmail.com>
2022-02-18 08:47:30 +0100

    extended  - pos

--- UTrans/run/run_all_nnformergt1.py
+++ UTrans/run/run_all_nnformergt1.py
@@ -2,9 +2,9 @@ import run_training as run
 
 g = '0'
 # run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2_nnFormerGT1gv2', task='017', fold=0, outpath='NNFORMERGT1af', val=False, npz=True)
-run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2_nnFormerGT1gv2', task='017', fold=0, outpath='NNFORMERGT1af', val=True,  npz=True)
-run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2_nnFormerGT1gv2', task='017', fold=1, outpath='NNFORMERGT1af', val=False, npz=True)
-run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2_nnFormerGT1gv2', task='017', fold=1, outpath='NNFORMERGT1af', val=True,  npz=True)
+# run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2_nnFormerGT1gv2', task='017', fold=0, outpath='NNFORMERGT1af', val=True,  npz=True)
+# run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2_nnFormerGT1gv2', task='017', fold=1, outpath='NNFORMERGT1af', val=False, npz=True)
+# run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2_nnFormerGT1gv2', task='017', fold=1, outpath='NNFORMERGT1af', val=True,  npz=True)
 run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2_nnFormerGT1gv2', task='017', fold=2, outpath='NNFORMERGT1af', val=False, npz=True)
 run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2_nnFormerGT1gv2', task='017', fold=2, outpath='NNFORMERGT1af', val=True,  npz=True)
 run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2_nnFormerGT1gv2', task='017', fold=3, outpath='NNFORMERGT1af', val=False, npz=True)
--- UTrans/run/run_all_unet.py
+++ UTrans/run/run_all_unet.py
@@ -4,8 +4,8 @@ import run_training as run
 g = '3'
 
 
-run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2', task='017', fold=0, outpath='NNUNETaf', val=False, npz=True, na=True)
-run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2', task='017', fold=0, outpath='NNUNETaf', val=True, npz=True, na=True)
+# run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2', task='017', fold=0, outpath='NNUNETaf', val=False, npz=True, na=True)
+# run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2', task='017', fold=0, outpath='NNUNETaf', val=True, npz=True, na=True)
 run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2', task='017', fold=1, outpath='NNUNETaf', val=False, npz=True, na=True)
 run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2', task='017', fold=1, outpath='NNUNETaf', val=True, npz=True, na=True)
 run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2', task='017', fold=2, outpath='NNUNETaf', val=False, npz=True, na=True)
