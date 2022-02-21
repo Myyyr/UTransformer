dc3cf645a4463639f010e6750d9f87e406828b38
MYR <loic.themyr@gmail.com>
2022-02-21 17:15:19 +0100

    extended  - pos

--- UTrans/run/task001_run_all_fine.py
+++ UTrans/run/task001_run_all_fine.py
@@ -1,5 +1,5 @@
 import run_training as run
 
 g = '0'
-run.main(gpu=g, network='3d_fullres', network_trainer='task001_nnUNetTrainerV2_nnFormerGT1EXTV6', task='001', fold=0, outpath='FINEV6', val=False, npz=True, dbg=True)
+run.main(gpu=g, network='3d_fullres', network_trainer='task001_nnUNetTrainerV2_nnFormerGT1EXTV6', task='001', fold=0, outpath='FINEV6', val=False, npz=True)
 run.main(gpu=g, network='3d_fullres', network_trainer='task001_nnUNetTrainerV2_nnFormerGT1EXTV6', task='001', fold=0, outpath='FINEV6', val=True,  npz=True)
