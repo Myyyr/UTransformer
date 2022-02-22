5bfaa008213e98a42f7bd23ab239c6a192d70f27
MYR <loic.themyr@gmail.com>
2022-02-22 11:56:31 +0100

    extended  - pos

--- UTrans/run/run_all_nnformerextgt1v7_x11.py
+++ UTrans/run/run_all_nnformerextgt1v7_x11.py
@@ -1,6 +1,6 @@
 import run_training as run
 
-g = '5'
+g = '0'
 run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2_nnFormerGT1EXTV7', task='017', fold=0, outpath='NNFORMEREXTGT1V7x5', val=False, npz=True, vt_map=(3,5,5,2), dbg=True)
 # run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2_nnFormerGT1EXTV7', task='017', fold=0, outpath='NNFORMEREXTGT1V7x5', val=True,  npz=True, vt_map=(3,5,5,1))
 # run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2_nnFormerGT1EXTV7', task='017', fold=1, outpath='NNFORMEREXTGT1V7x5', val=False, npz=True, vt_map=(3,5,5,1))
