538324f8d69268a0e097cc7ebff13c111a8c9970
MYR <loic.themyr@gmail.com>
2022-02-23 15:13:40 +0100

    extended  - pos

--- UTrans/network_architecture/nnformergt_ext_v7.py
+++ UTrans/network_architecture/nnformergt_ext_v7.py
@@ -386,13 +386,14 @@ class SwinTransformerBlock(nn.Module):
         if B==2:
             vt_pos_[self.n_vts:] = [self.nc+vt_pos_[self.n_vts + i] for i in range(self.n_vts)]
 
-        # print("-----------------DBG-----------------")
-        # print("nc", self.nc)
-        # print("vt_pos", vt_pos)
-        # print("vt_pos_", vt_pos_)
-        # print("vts.shape", vts.shape)
-        # print("-------------------------------------")
+        print("-----------------DBG-----------------")
+        print("nc", self.nc)
+        print("vt_pos", vt_pos)
+        print("vt_pos_", vt_pos_)
+        print("self.n_vts", self.n_vts)
         vts = rearrange(vts, "b n c -> (b n) c")
+        print("vts.shape", vts.shape)
+        print("-------------------------------------")
         vt  = vts[vt_pos_]
         vt  = rearrange(vt, "(b n) c -> b n c", b=B)
         # vts = rearrange(vts, "(b n) c -> b n c", b=B)
