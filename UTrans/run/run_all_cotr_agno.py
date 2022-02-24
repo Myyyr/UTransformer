e92fa26a5d14549aa52cca7401a6a3e2689a4f1d
MYR <loic.themyr@gmail.com>
2022-02-24 19:08:17 +0100

    extended  - pos

--- UTrans/run/run_all_cotr_agno.py
+++ UTrans/run/run_all_cotr_agno.py
@@ -1,12 +1,20 @@
 import run_training as run
 
 # g = '5'
-g = '2'
+g = '0'
 # run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2_CoTR_agno', task='017', fold=0, outpath='COTR_agno', val=False, npz=True, na=True)
 # run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2_CoTR_agno', task='017', fold=0, outpath='COTR_agno', val=True, npz=True, na=True)
 # run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2_CoTR_agno', task='017', fold=1, outpath='COTR_agno', val=False, npz=True, na=True)
 # run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2_CoTR_agno', task='017', fold=1, outpath='COTR_agno', val=True, npz=True, na=True)
 # run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2_CoTR_agno', task='017', fold=2, outpath='COTR_agno', val=False, npz=True, na=True)
 # run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2_CoTR_agno', task='017', fold=2, outpath='COTR_agno', val=True, npz=True, na=True)
-run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2_CoTR_agno', task='017', fold=3, outpath='COTR_agno', val=False, npz=True, na=True)
-run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2_CoTR_agno', task='017', fold=3, outpath='COTR_agno', val=True, npz=True, na=True)
+# run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2_CoTR_agno', task='017', fold=3, outpath='COTR_agno', val=False, npz=True, na=True)
+# run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2_CoTR_agno', task='017', fold=3, outpath='COTR_agno', val=True, npz=True, na=True)
+
+
+run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2_CoTR_agno', task='017', fold=1, outpath='nnffCOTR_agno', val=False, npz=True, na=True)
+run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2_CoTR_agno', task='017', fold=1, outpath='nnffCOTR_agno', val=True, npz=True, na=True)
+run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2_CoTR_agno', task='017', fold=2, outpath='nnffCOTR_agno', val=False, npz=True, na=True)
+run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2_CoTR_agno', task='017', fold=2, outpath='nnffCOTR_agno', val=True, npz=True, na=True)
+run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2_CoTR_agno', task='017', fold=3, outpath='nnffCOTR_agno', val=False, npz=True, na=True)
+run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2_CoTR_agno', task='017', fold=3, outpath='nnffCOTR_agno', val=True, npz=True, na=True)
--- UTrans/run/run_all_nnformerextgt1v6.py
+++ UTrans/run/run_all_nnformerextgt1v6.py
@@ -1,15 +1,24 @@
 import run_training as run
 
-g = '4'
+g = '1'
 # run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2_nnFormerGT1EXTV6', task='017', fold=0, outpath='NNFORMEREXTGT1V6', val=False, npz=True)
 # run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2_nnFormerGT1EXTV6', task='017', fold=0, outpath='NNFORMEREXTGT1V6', val=True,  npz=True)
 # run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2_nnFormerGT1EXTV6', task='017', fold=0, outpath='NNFORMEREXTGT1V6_deter', val=False, npz=True)
 # run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2_nnFormerGT1EXTV6', task='017', fold=0, outpath='NNFORMEREXTGT1V6_deter', val=True,  npz=True)
 # run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2_nnFormerGT1EXTV6', task='017', fold=0, outpath='NNFORMEREXTGT1V6af', val=False, npz=True)
 # run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2_nnFormerGT1EXTV6', task='017', fold=0, outpath='NNFORMEREXTGT1V6af', val=True,  npz=True)
-run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2_nnFormerGT1EXTV6', task='017', fold=1, outpath='NNFORMEREXTGT1V6af', val=False, npz=True)
-run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2_nnFormerGT1EXTV6', task='017', fold=1, outpath='NNFORMEREXTGT1V6af', val=True,  npz=True)
-run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2_nnFormerGT1EXTV6', task='017', fold=2, outpath='NNFORMEREXTGT1V6af', val=False, npz=True)
-run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2_nnFormerGT1EXTV6', task='017', fold=2, outpath='NNFORMEREXTGT1V6af', val=True,  npz=True)
-run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2_nnFormerGT1EXTV6', task='017', fold=3, outpath='NNFORMEREXTGT1V6af', val=False, npz=True)
-run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2_nnFormerGT1EXTV6', task='017', fold=3, outpath='NNFORMEREXTGT1V6af', val=True,  npz=True)
+# run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2_nnFormerGT1EXTV6', task='017', fold=1, outpath='NNFORMEREXTGT1V6af', val=False, npz=True)
+# run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2_nnFormerGT1EXTV6', task='017', fold=1, outpath='NNFORMEREXTGT1V6af', val=True,  npz=True)
+# run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2_nnFormerGT1EXTV6', task='017', fold=2, outpath='NNFORMEREXTGT1V6af', val=False, npz=True)
+# run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2_nnFormerGT1EXTV6', task='017', fold=2, outpath='NNFORMEREXTGT1V6af', val=True,  npz=True)
+# run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2_nnFormerGT1EXTV6', task='017', fold=3, outpath='NNFORMEREXTGT1V6af', val=False, npz=True)
+# run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2_nnFormerGT1EXTV6', task='017', fold=3, outpath='NNFORMEREXTGT1V6af', val=True,  npz=True)
+
+
+
+run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2_nnFormerGT1EXTV6', task='017', fold=1, outpath='nnffNNFORMEREXTGT1V6', val=False, npz=True)
+run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2_nnFormerGT1EXTV6', task='017', fold=1, outpath='nnffNNFORMEREXTGT1V6', val=True,  npz=True)
+run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2_nnFormerGT1EXTV6', task='017', fold=2, outpath='nnffNNFORMEREXTGT1V6', val=False, npz=True)
+run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2_nnFormerGT1EXTV6', task='017', fold=2, outpath='nnffNNFORMEREXTGT1V6', val=True,  npz=True)
+run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2_nnFormerGT1EXTV6', task='017', fold=3, outpath='nnffNNFORMEREXTGT1V6', val=False, npz=True)
+run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2_nnFormerGT1EXTV6', task='017', fold=3, outpath='nnffNNFORMEREXTGT1V6', val=True,  npz=True)
