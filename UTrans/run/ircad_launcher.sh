be7cca82211f670a9738ce2e56fb59f016eb3b6a
MYR <loic.themyr@gmail.com>
2022-02-24 10:26:22 +0100

    extended  - pos

--- UTrans/run/ircad_launcher.sh
+++ UTrans/run/ircad_launcher.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH -p public
-#SBATCH --gpus=1
+#SBATCH --gpus=0
 #SBATCH --partition=long
-#SBATCH --time=100:00:00 
+#SBATCH --time=01:00:00 
 #SBATCH --output=logs/nnfbraintumor.out # output file name
 #SBATCH --error=logs/nnfbraintumor.err  # error file name
 
@@ -16,14 +16,18 @@ source /home/lthemyr/cotr/bin/activate
 
 
 
-## PROCESS TASK001
+### PROCESS TASK001
+## TRAIN
+
 # srun nnUNet_convert_decathlon_task -i /scratch/lthemyr/Task01_BrainTumour -p 8
 # srun nnUNet_plan_and_preprocess -t 001 --verify_dataset_integrity
 
-
+## EVAL
+cd /home/lthemyr/UTransformer/UTrans/inference
+srun python tumour.py /scratch/lthemyr
 
 ## Train nnf brats 
-cd /home/lthemyr/UTransformer/UTrans/run
+# cd /home/lthemyr/UTransformer/UTrans/run
 # srun python brats_run_all_nnformergt1.py
 # srun python task001_run_all_fine.py
-srun python task001_run_all_nnformer.py
+# srun python task001_run_all_nnformer.py
