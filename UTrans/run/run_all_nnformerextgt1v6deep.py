32c8f9f37e43693e20990c91d7f0118d475e08ed
MYR <loic.themyr@gmail.com>
2022-02-25 15:31:18 +0100

    extended  - pos

--- UTrans/run/run_all_nnformerextgt1v6deep.py
+++ UTrans/run/run_all_nnformerextgt1v6deep.py
@@ -1,5 +1,5 @@
 import run_training as run
 
 g = '0'
-run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2_nnFormerGT1EXTV6Deep', task='017', fold=0, outpath='FINEDEEP', val=False, npz=True, dbg=True)
-# run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2_nnFormerGT1EXTV6', task='017', fold=0, outpath='FINEDEEP', val=True,  npz=True)
+run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2_nnFormerGT1EXTV6Deep', task='017', fold=0, outpath='FINEDEEP', val=False, npz=True)
+run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2_nnFormerGT1EXTV6', task='017', fold=0, outpath='FINEDEEP', val=True,  npz=True)
