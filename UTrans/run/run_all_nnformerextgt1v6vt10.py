cb56e2b175881fe3d302b755861065d049ecfc0a
MYR <loic.themyr@gmail.com>
2022-02-25 16:45:25 +0100

    extended  - pos

--- UTrans/run/run_all_nnformerextgt1v6vt10.py
+++ UTrans/run/run_all_nnformerextgt1v6vt10.py
@@ -4,5 +4,5 @@ g = '1'
 
 
 
-run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2_nnFormerGT1EXTV6VT10', task='017', fold=0, outpath='FINEVT10', val=False, npz=True, dbg=True)
+run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2_nnFormerGT1EXTV6VT10', task='017', fold=0, outpath='FINEVT10', val=False, npz=True)
 run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2_nnFormerGT1EXTV6VT10', task='017', fold=0, outpath='FINEVT10', val=True,  npz=True)
