9c6eb7d7eb72045bea73ebead8346d99d7d54625
MYR <loic.themyr@gmail.com>
2022-02-27 22:07:32 +0100

    extended  - pos

--- UTrans/run/ircad_launcher.sh
+++ UTrans/run/ircad_launcher.sh
@@ -16,19 +16,21 @@ source /home/lthemyr/cotr/bin/activate
 
 
 
-### PROCESS TASK001
-## TRAIN
-
+## PROCESS TASK001
 # srun nnUNet_convert_decathlon_task -i /scratch/lthemyr/Task01_BrainTumour -p 8
 # srun nnUNet_plan_and_preprocess -t 001 --verify_dataset_integrity
 
 ## EVAL
-# cd /home/lthemyr/UTransformer/UTrans/inference
-# srun python tumour.py /scratch/lthemyr FINEV6_IN_LeakyReLU
+cd /home/lthemyr/UTransformer/UTrans/inference
+srun python tumour.py /scratch/lthemyr FINEV6_2_IN_LeakyReLU
 # srun python tumour.py /scratch/lthemyr NNFORMER_IN_LeakyReLU
 
-## Train nnf brats 
-cd /home/lthemyr/UTransformer/UTrans/run
-# srun python brats_run_all_nnformergt1.py
-srun python task001_run_all_fine.py
+## TRAIN
+# cd /home/lthemyr/UTransformer/UTrans/run
+# srun python task001_run_all_fine.py
 # srun python task001_run_all_nnformer.py
+
+
+
+## Train nnf brats 
+# srun python brats_run_all_nnformergt1.py
