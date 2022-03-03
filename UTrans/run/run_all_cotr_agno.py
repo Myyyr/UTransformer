484ba31809ecaff2577045b95323191cbe8070b9
MYR <loic.themyr@gmail.com>
2022-03-03 15:22:03 +0100

    extended  - pos

--- UTrans/run/run_all_cotr_agno.py
+++ UTrans/run/run_all_cotr_agno.py
@@ -12,15 +12,15 @@ import run_training as run
 
 
 # g = '2'
-g = '5'
-# g = '0'
+# g = '5'
+g = '0'
 
 # run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2_CoTR_agno', task='017', fold=2, outpath='nnffCOTR_agno', val=False, npz=True, na=True)
 # run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2_CoTR_agno', task='017', fold=2, outpath='nnffCOTR_agno', val=True, npz=True, na=True)
-run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2_CoTR_agno', task='017', fold=3, outpath='nnffCOTR_agno', val=False, npz=True, na=True)
-run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2_CoTR_agno', task='017', fold=3, outpath='nnffCOTR_agno', val=True, npz=True, na=True)
-# run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2_CoTR_agno', task='017', fold=1, outpath='nnffCOTR_agno', val=False, npz=True, na=True)
-# run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2_CoTR_agno', task='017', fold=1, outpath='nnffCOTR_agno', val=True, npz=True, na=True)
+# run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2_CoTR_agno', task='017', fold=3, outpath='nnffCOTR_agno', val=False, npz=True, na=True)
+# run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2_CoTR_agno', task='017', fold=3, outpath='nnffCOTR_agno', val=True, npz=True, na=True)
+run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2_CoTR_agno', task='017', fold=1, outpath='nnffCOTR_agno', val=False, npz=True, na=True)
+run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2_CoTR_agno', task='017', fold=1, outpath='nnffCOTR_agno', val=True, npz=True, na=True)
 
 
 # g='1'
