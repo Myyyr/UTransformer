45096e2db097b59932b4d723ca848bb3c17a1761
MYR <loic.themyr@gmail.com>
2022-03-03 10:45:57 +0100

    extended  - pos

--- UTrans/run/run_all_nnformer.py
+++ UTrans/run/run_all_nnformer.py
@@ -1,6 +1,6 @@
 import run_training as run
 
-g = '3'
+g = '0'
 # # run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2_nnFormer', task='017', fold=0, outpath='NNFORMERaf', val=False, npz=True)
 # # run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2_nnFormer', task='017', fold=0, outpath='NNFORMERaf', val=True,  npz=True)
 # # run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2_nnFormer', task='017', fold=1, outpath='NNFORMERaf', val=False, npz=True)
