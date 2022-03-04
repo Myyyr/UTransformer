52b303a0218eee6b218d627b0ccfdb50a14b4e5e
MYR <loic.themyr@gmail.com>
2022-03-04 12:54:41 +0100

    extended  - pos

--- UTrans/run/ircad_run_all_cotr_agno.py
+++ UTrans/run/ircad_run_all_cotr_agno.py
@@ -4,5 +4,5 @@ import run_training as run
 
 g = '0'
 
-run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2_CoTR_agno', task='017', fold=2, outpath='nnffCOTR_agno', val=False, npz=True, na=True)
-run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2_CoTR_agno', task='017', fold=2, outpath='nnffCOTR_agno', val=True, npz=True, na=True)
+run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2_CoTR_agno', task='017', fold=0, outpath='nnffCOTR_agno', val=False, npz=True, na=True)
+run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2_CoTR_agno', task='017', fold=0, outpath='nnffCOTR_agno', val=True, npz=True, na=True)
