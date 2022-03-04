60e8669c8633718283ef6b64edf48f857a560293
MYR <loic.themyr@gmail.com>
2022-03-04 12:44:17 +0100

    extended  - pos

--- UTrans/run/_to_run_cotr_f1.py
+++ UTrans/run/_to_run_cotr_f1.py
@@ -1,5 +1,6 @@
 import run_training as run
 # m2n
+# F4 -> F1 (nnf file)
 g='3'
-run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2_CoTR_agno', task='017', fold=1, outpath='COTR_sup', val=False, npz=True)
-run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2_CoTR_agno', task='017', fold=1, outpath='COTR_sup', val=True,  npz=True)
+run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2_CoTR_agno', task='017', fold=4, outpath='COTR_sup', val=False, npz=True, na=True)
+run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2_CoTR_agno', task='017', fold=4, outpath='COTR_sup', val=True,  npz=True), na=True)
