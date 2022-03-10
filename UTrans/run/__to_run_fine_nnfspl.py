2e35f7d142e89b6dbaf17a5db5880d0ab82c6e0d
MYR <loic.themyr@gmail.com>
2022-03-10 11:31:50 +0100

    extended  - pos

--- UTrans/run/__to_run_fine_nnfspl.py
+++ UTrans/run/__to_run_fine_nnfspl.py
@@ -1,6 +1,6 @@
 import run_training as run
 # ircad
-# fine
+# fine_nnf
 g='0'
 run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2_nnFormerGT1EXTV6', task='017', fold=1, outpath='FINE_nnspl', val=False, npz=True)
 run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2_nnFormerGT1EXTV6', task='017', fold=1, outpath='FINE_nnspl', val=True,  npz=True)
