32a003c62e4990a163ae0dda50add40b54346876
MYR <loic.themyr@gmail.com>
2022-03-17 15:13:15 +0100

    nnf

---
+++ UTrans/run/task001_run_all_nnformer_official.py
@@ -0,0 +1,5 @@
+import run_training as run
+
+g = '0'
+run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2_nnFormer_Official', task='001', fold=0, outpath='NNFORMEROff', val=False, npz=True)
+run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2_nnFormer_Official', task='001', fold=0, outpath='NNFORMEROff', val=True,  npz=True)
